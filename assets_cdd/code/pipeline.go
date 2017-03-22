c6dae4e48a3731ab4cd007b04df20481501e0e1c
girishramnani <girishramnani95@gmail.com>
2017-03-23 00:21:52 +0530

    added the concurrency presentation

---
+++ assets_cdd/code/pipeline.go
@@ -0,0 +1,34 @@
+package main
+
+
+func generate(nums ...int) <-chan int {
+    out := make(chan int)
+    go func() {
+        for _, n := range nums {
+            out <- n
+        }
+        close(out)
+    }()
+    return out
+}
+
+func pipe(in <-chan int) <-chan int {
+    out := make(chan int)
+    go func() {
+        for n := range in {
+            out <- n * n
+        }
+        close(out)
+    }()
+    return out
+}
+
+func main() {
+    // create the pipeline
+    c := generate(2, 3)
+    out := pipe(c)
+
+    // Consume the output.
+    fmt.Println(<-out) // 4
+    fmt.Println(<-out) // 9
+}
